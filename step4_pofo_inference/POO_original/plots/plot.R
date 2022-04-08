48027d0ea9024030be65215f70e595f9cb61bbbe
RJHFMSTR <89519497+RJHFMSTR@users.noreply.github.com>
2022-04-08 17:44:11 +0200

    Delete step4_pofo_inference directory

--- step4_pofo_inference/POO_original/plots/plot.R
+++
@@ -1,6 +0,0 @@
-library(RColorBrewer)
-COL = brewer.pal(8,"Set1")
-
-
-D= read.table("test2", head=FALSE, stringsAsFactors=FALSE)
-
--- step4_pofo_inference/step1_scaffold.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CHROM=20
-
-python src/scaffold.py -chr $CHROM -cm 3.0
-
-
